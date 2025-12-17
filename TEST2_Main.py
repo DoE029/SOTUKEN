291ba8e7a2b0502237c318d6ab3886821cf06479
karasawa <>
2025-12-17 11:59:34 +0900

    コメントの追加。スキャン感覚の数値

--- TEST2_Main.py
+++ TEST2_Main.py
@@ -51,7 +51,7 @@ async def buzzer_task(target_ids):
             
         try:
             # タスクがキャンセルされた場合に備えて await を try-except に入れる
-            await asyncio.sleep(1) # 2秒ごとにチェック
+            await asyncio.sleep(1) # ここいじるとブザーの感覚が狭まる。たぶんスキャン感覚。
         except asyncio.CancelledError:
             # キャンセルされたらループを抜ける
             print("ブザー警告停止。")
