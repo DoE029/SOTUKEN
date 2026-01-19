c191e7822a9eb8d53ac4b1b40ec69e0b03c9f7fc
karasawa <>
2026-01-19 10:02:39 +0900

    NEWの時間変更

--- TEST_TIME_MAIN.py
+++ TEST_TIME_MAIN.py
@@ -2,8 +2,8 @@ import asyncio
 import datetime
 import json
 import os
-from BLE_beacon_v3 import scan_beacon
-import LED_Buzzer_v5 as gpio
+from BLE_New_beacon import scan_beacon
+import LED_New_Buzzer as gpio
 
 # --- ⚙️ 設定項目 ---
 SCHEDULED_TIME = "14:00"  # 毎日この時間になったら実行
